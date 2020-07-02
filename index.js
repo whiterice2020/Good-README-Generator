10143a7b508f094ad2a05990dc68ab552bac2d55
Paul DelPadre <PDelPadre@rice.local>
2020-07-01 22:52:06 -0400

    Updating

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
+
+
+
+// ## Acceptance Criteria
+
+// ```md
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository
+// THEN a quality, professional README.md is generated with the title of your project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added hear the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
+// ```
+
+// ## Minimum Application Requirements
+
+// * Meets [Submission Requirements](#submission-requirements) in the following section.
+
+// * Functional application.
+
+// * GitHub repository with a unique name and a README describing project.
+
+// * The generated README includes the following sections: 
+
+//   * Title
+//   * Description
+//   * Table of Contents
+//   * Installation
+//   * Usage
+//   * License
+//   * Contributing
+//   * Tests
+//   * Questions
+
+// * The generated README includes 1 badge that's specific to the repository.
+
+
 // array of questions for user
 const questions = [
 
@@ -14,3 +60,7 @@ function init() {
 
 // function call to initialize program
 init();
+
+
+
+
