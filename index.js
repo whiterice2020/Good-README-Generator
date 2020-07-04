c3099c8c780a3120ba3ab5ade476b08b3e471d7f
Paul DelPadre <PDelPadre@rice.local>
2020-07-04 18:43:59 -0400

    Cleaning up comments

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
-// ```
-
-
-
-// * The generated README includes 1 badge that's specific to the repository.
 var inquirer = require("inquirer");
 var fs = require('fs');
 const generateMarkdown = require("./utils/generateMarkdown")
 
-// inquirer.prompt([
   const questions = [
   {
     type: "input",
